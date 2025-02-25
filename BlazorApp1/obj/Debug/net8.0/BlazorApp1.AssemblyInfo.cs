07df3aad9893cac2b0a068df932afb842dcad37b
Gabriel Iuza Frost <109495931+gabroisreal@users.noreply.github.com>
2025-02-25 11:00:02 -0600

    almost done with blazor 5

--- BlazorApp1/obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
+++ BlazorApp1/obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f1b309aaf02a48e0203cd628f0912655df316c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bcd35cee0e46fa435cc2aab3dedd88ccdfacf61")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
