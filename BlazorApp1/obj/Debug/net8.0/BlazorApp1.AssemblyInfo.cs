0f1b309aaf02a48e0203cd628f0912655df316c9
Gabriel Iuza Frost <109495931+gabroisreal@users.noreply.github.com>
2025-02-24 19:09:59 -0600

    blazor 2 done????

--- BlazorApp1/obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
+++ BlazorApp1/obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45152be4e23daafa486d48e920cfcb9537ada415")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c215725ea6aa128f79add98a779567f56ffa1041")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
