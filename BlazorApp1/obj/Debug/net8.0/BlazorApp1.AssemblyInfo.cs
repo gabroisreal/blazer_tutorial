5f0972e849c82049445087cc1794631ca2ad39b1
Gabriel Iuza Frost <109495931+gabroisreal@users.noreply.github.com>
2025-02-27 10:45:08 -0600

    somethings

--- BlazorApp1/obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
+++ BlazorApp1/obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c456f50a7cd049eb2d4caeffd10a14b7eb9aec84")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5e1e262557a4d9dfba57281b9720d0891ac2b48")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
